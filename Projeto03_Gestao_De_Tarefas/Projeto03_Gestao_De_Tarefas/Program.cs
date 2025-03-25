59a91512ffc417a1777fb6f2f69709272af3dd71
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-03-25 20:56:33 -0300

    First Commit
    Subindo o ínicio do projeto

---
+++ Projeto03_Gestao_De_Tarefas/Projeto03_Gestao_De_Tarefas/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Projeto03_Gestao_De_Tarefas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To do list");
+            string Menu = @"
+1 - Novo responsável
+2 - Nova tarefa
+3 - Excluir tarefa
+4 - Atualizar tarefa
+5.1 - Lista tarefas
+5.2 - Listar tarefas pendentes
+5.3 - Listar tarefas concluidas
+6.1 - Listar tarefas por responsável
+6.2 - Listar tarefas pendentes por responsável
+6.3 - Listar tarefas concluidas por responsável
+0 - Encerrar programa";
+
+            string opcao = "0";
+
+            do
+            {
+                Console.WriteLine(Menu);
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+
+                }
+
+            } while (opcao != "0");
+        }
+
+        
+    }
+}
---
+++ Projeto03_Gestao_De_Tarefas/Projeto03_Gestao_De_Tarefas/Responsavel_Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto03_Gestao_De_Tarefas
+{
+    internal class Responsavel
+    {
+        public string Nome { get;}
+
+        public string Email { get; set; }
+    }
+}
+
+
+
+
+
